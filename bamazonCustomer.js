bd564e37e771562daea5f54f3483ac5381169604
paredfed <paredfed@gmail.com>
2018-11-03 17:30:17 -0500

    challenges 1 and 2

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,10 @@
 const inquirer = require("inquirer");
 require("dotenv").config();
-var mysql = require('mysql');
+const mysql = require('mysql');
+const cTable = require('console.table');
+let regex;
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || "root",
@@ -14,88 +16,86 @@ connection.connect(function (err) {
     if (err) {
         throw ('error connecting: ' + err.stack);
     }
-    console.log('connected as id ' + connection.threadId);
-
-    connection.query('SELECT * FROM `products`', (error, results)=> {
-        // error will be an Error if one occurred during the query
-        if(error) throw error;
-        // results will contain the results of the query
-        // fields will contain information about the returned results fields (if any)
-        results.forEach((row)=>console.log(row.product_name));
-      });
-
-
-
-    connection.end(function (err) {
-        console.log("BYE");
-    
-    });
+    readProducts();
 });
 
-
-function createProduct() {
-    console.log("Inserting a new product...\n");
-    var query = connection.query(
-        "INSERT INTO products SET ?",
-        {
-            title: "Highway to Hell",
-            artist: "AC/DC",
-            genre: "metal"
-        },
-        function (err, res) {
-            if(err) throw err;
-            console.log(res.affectedRows + " product inserted!\n");
-            // Call updateProduct AFTER the INSERT completes
-        }
-    );
-    console.log(query.sql);
-}
-
-
-function updateProduct() {
-    console.log("Updating all ???????????...\n");
-    var query = connection.query(
-      "UPDATE products SET ? WHERE ?",
-      [
-        {
-          genre: "metal"
-        },
-        {
-          artist: "weezer"
-        }
-      ],
-      function(err, res) {
-          if(err) throw err;
-        console.log(res.affectedRows + " products updated!\n");
-      }
-    );
-  
-    // logs the actual query being run
-    console.log(query.sql);
-  }
-  
-
-function deleteProduct() {
-    console.log("Deleting all metal songs...\n");
-    connection.query(
-        "DELETE FROM products WHERE ?",
-        {
-            genre: "metal"
-        },
-        function (err, res) {
-            if(err) throw err;
-            console.log(res.affectedRows + " songs deleted!\n");
-        }
-    );
-}
-
 function readProducts() {
-    console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function (err, res) {
+    connection.query("SELECT item_id,product_name,department_name,price FROM products", function (err, res) {
         if (err) throw err;
-        // Log all results of the SELECT statement
-        console.log(res);
-        connection.end();
+        console.table(res);
+        regex = new RegExp(`^([1-9]|${res.length})$`);
+        start();
     });
 }
 
+function start() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "buy",
+            message: "Input the ID of what you would like to buy",
+            validate: function (value) {
+                const pass = value.match(regex);
+                if (pass) {
+                    return true;
+                }
+                return "Please select a valid ID";
+            }
+        },
+        {
+            type: "input",
+            name: "amt",
+            message: "How many?"
+        }
+    ]).then((ans) => {
+        connection.query(
+            "SELECT * FROM products WHERE ?",
+            {
+                item_id: parseInt(ans.buy)
+            },
+            function (err, res) {
+                if (err) throw err;
+                if (parseInt(res[0].stock_quantity) < parseInt(ans.amt)) {
+                    console.log("Insufficient quantity!");
+                    return readProducts();
+                }
+                updateProduct(res[0].stock_quantity - parseInt(ans.amt), parseInt(ans.buy), res[0].price * parseInt(ans.amt)+res[0].product_sales);
+
+            }
+        );
+    });
+}
+
+function end() {
+    connection.end(function (err) {
+        console.log("BYE");
+    });
+}
+
+function updateProduct(toSet, whatId, cost) {
+    connection.query("UPDATE products SET ? WHERE ?",[
+            {
+                stock_quantity: toSet,
+                product_sales: cost
+            },
+            {
+                item_id: whatId
+            },
+        ],
+        function (err, res) {
+            if (err) throw err;
+            console.log("\n" + cost + " dollars is how much you spent!\n");
+            inquirer.prompt([
+                {
+                    type: "confirm",
+                    name: "restart",
+                    message: "Buy something else?"
+                }
+            ]).then(ans => {
+                if (ans.restart)
+                    return readProducts();
+                return end();
+            });
+        }
+    );
+}
